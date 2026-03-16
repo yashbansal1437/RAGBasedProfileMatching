ca49f7edc10b0fb79408cd908caa7c4d4b1a8859
Yash Bansal <89179197+yashbansal1437@users.noreply.github.com>
2026-03-16 20:21:17 +0530

    Add chunk_document function to split text

---
+++ chunker.py
@@ -0,0 +1,12 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from src.config import CHUNK_SIZE, CHUNK_OVERLAP
+
+
+def chunk_document(text):
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=CHUNK_OVERLAP
+    )
+
+    return splitter.split_text(text)
