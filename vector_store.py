783efaeece2fad6afbd14faed860d9876c83cc88
Yash Bansal <89179197+yashbansal1437@users.noreply.github.com>
2026-03-16 20:22:49 +0530

    Implement store_embeddings function in vector_store.py

---
+++ vector_store.py
@@ -0,0 +1,24 @@
+import chromadb
+from src.config import VECTOR_DB_NAME
+
+client = chromadb.Client()
+
+collection = client.get_or_create_collection(
+    name=VECTOR_DB_NAME
+)
+
+
+def store_embeddings(chunks, embeddings, metadata, file_name):
+
+    for i, chunk in enumerate(chunks):
+
+        collection.add(
+            documents=[chunk],
+            embeddings=[embeddings[i].tolist()],
+            metadatas=[{
+                "skills": ",".join(metadata["skills"]),
+                "experience": metadata["experience"],
+                "file": file_name
+            }],
+            ids=[f"{file_name}_{i}"]
+        )
